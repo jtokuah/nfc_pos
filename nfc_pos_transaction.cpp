82d9585c9079e2f7a87343e74857d28058774945
jefferson <jeff.tokurah@gmail.com>
2013-08-10 06:17:57 -0700

    Writing transaction protocol

--- nfc_pos_transaction.cpp
+++ nfc_pos_transaction.cpp
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_NFCShield_I2C.h>
-
+#include <errno.h>
 
 #include "nfc_pos.h"
 
@@ -9,40 +9,53 @@
 
 Adafruit_NFCShield_I2C nfc(IRQ, RESET);
 
-void nfc_pos_chip_config(void){
+static int transaction_ID = -1;
+
+/*
+ * Configure NFC board
+ */
+void nfc_pos_configure_board(void){
 	Serial.begin(115200);
 	nfc.begin();
 
 	uint32_t versiondata = nfc.getFirmwareVersion();
 	if (! versiondata) {
-	Serial.print("Didn't find PN53x board");
+	ERRORF("Didn't find PN53x board");
 	while (1); // halt
 	}
 	// Got ok data, print it out!
-	Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
-	Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
-	Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  	Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
+  	Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
+  	Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
 
 	// configure board to read RFID tags and cards
 	nfc.SAMConfig();
 }
 
+/*
+ * Create unique transaction ID
+ */
+int nfc_pos_generate_transaction_ID(){
+
+}
+
 int nfc_pos_transact(int paymentAmount) {
 	int status = 0;
 
-	 nfc_pos_chip_config();
 
-	 char DataOut1[]="Connecting to server...";
-	 char DataOut2[] = "";
-	 sprintf(DataOut2, "Authenticating $ %d", paymentAmount);
-	 char DataOut3[]="Payment completed";
-	 char DataIn1[sizeof(DataOut1)];
-	 char DataIn2[sizeof(DataOut2)];
-	 char DataIn3[sizeof(DataOut3)];
+	nfc_pos_configure_board();
+
+	char DataOut1[]="Connecting to server...";
+	char DataOut2[] = "";
+	sprintf(DataOut2, "Authenticating $ %d", paymentAmount);
+	char DataOut3[]="Payment completed";
+	char DataIn1[sizeof(DataOut1)];
+	char DataIn2[sizeof(DataOut2)];
+	char DataIn3[sizeof(DataOut3)];
 
 	 // Configure PN532 as Peer to Peer Target
-	 if(nfc.inListPassiveTarget()) //if connection is error-free
-	 {
+	if(nfc.inListPassiveTarget()) //if connection is error-free
+	{
 		//trans-receive data
 		if(nfc.inDataExchange(DataOut1, (uint8_t)sizeof(DataOut1), DataIn1, (uint8_t*)sizeof(DataIn1)))
 		{
